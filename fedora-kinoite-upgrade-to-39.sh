6dc8d162dfacb69c6b95af44f63731213bc2ba12
BurningPho3nix <pr@burningpho3nix.xyz>
2023-10-06 15:23:15 +0200

    Update fedora-kinoite-upgrade-to-39.sh

--- fedora-kinoite-upgrade-to-39.sh
+++ fedora-kinoite-upgrade-to-39.sh
@@ -4,4 +4,4 @@ echo THIS SCRIPT WILL REBOOT YOUR PC AFTER THE REBASE
 
 sudo rpm-ostree upgrade
 sudo rpm-ostree rebase fedora:fedora/39/x86_64/kinoite
-sudo dnf -y system-upgrade reboot
+sudo reboot
